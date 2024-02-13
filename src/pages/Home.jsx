be486f6d3343b7a9b40b527a1bdb592f295ac9b2
jmcord <jcorderodz@hotmail.com>
2024-02-13 13:12:08 +0100

    c

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,12 @@
 //import ClaimRewardsForm from '../components/ClaimRewardsForm'
 
 
+
 export default function Home() {
   return (
     <section className="flex flex-col place-items-center gap-4 py-20 px-20">
       <h1>Blockmaker ERC20 Token</h1>
-    
+ 
     
       
     </section>
